cb5e8c155670edd1e1bd9ef4364ac0e6d0e0617b
Richard Whittington <richardlwhittington@gmail.com>
2022-11-08 21:16:00 -0600

    add cool face api

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const https = require("https");
 const request = require("request");
 
 const app = express();
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -91,7 +103,7 @@ app.post("/failure",function(req,res){
 
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is listening on port 3000");
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
 
 //7a89b11209 audience id
