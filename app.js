bcf85ffffd1f36b687a123a7191f158419bc8fd8
Richard Whittington <richardlwhittington@gmail.com>
2022-11-08 21:50:05 -0600

    added procfile

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const request = require("request");
 
-const app = express();
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// const app = express();
+// const cool = require('cool-ascii-faces');
+// const express = require('express');
+// const path = require('path');
+// const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
